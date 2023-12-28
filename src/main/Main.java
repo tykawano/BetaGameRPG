7e6bc0cfbd3fedd44247c4f22de2b55be9e15d90
Taiki <2002taikawa@gmail.com>
2023-12-28 00:58:35 -0700

    testing

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,5 +6,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         new GameFrame();
+        System.out.println("Hello testing");
     }
 }
